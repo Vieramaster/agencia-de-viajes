2faabb789073f44689931f81cdfafa8871e41e20
martin viera <118699912+Vieramaster@users.noreply.github.com>
2023-10-14 19:55:34 -0300

    work

--- lala.js
+++ lala.js
@@ -1,3 +1,21 @@
 const arrowCarrouselLeft = document.querySelector("#leftArrow")
-const arrowCarrouselright = document.querySelector("#rightArrow")
+const arrowCarrouselRight = document.querySelector("#rightArrow")
+const carrousel = document.querySelector(".block1__carrousel")
+const cards = document.querySelectorAll(".block1__card")
 
+let initial = 0;
+let widthCarrousel = 100 / cards.length;
+
+arrowCarrouselRight.addEventListener("click", () => {
+    if(initial < cards.length - 4){
+        initial++;
+        carrousel.style.transform = `translateX(-${initial * widthCarrousel}%)`;
+    }
+});
+
+arrowCarrouselLeft.addEventListener("click", ()=>{
+    if(initial > 0){
+        initial--;
+        carrousel.style.transform = `translateX(-${initial * widthCarrousel}%)`;
+    }
+});
