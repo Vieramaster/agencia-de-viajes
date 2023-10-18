9e8b0b4fcab6b271f096c6092db461467fa8f155
martin viera <118699912+Vieramaster@users.noreply.github.com>
2023-10-18 18:26:36 -0300

    working

--- lala.js
+++ lala.js
@@ -4,18 +4,19 @@ const carrousel = document.querySelector(".block1__carrousel")
 const cards = document.querySelectorAll(".block1__card")
 
 let initial = 0;
-let widthCarrousel = 100 / cards.length;
 
 arrowCarrouselRight.addEventListener("click", () => {
+    let cardWidth = cards[0].offsetWidth; // Recalcula el ancho de la tarjeta
     if(initial < cards.length - 4){
         initial++;
-        carrousel.style.transform = `translateX(-${initial * widthCarrousel}%)`;
+        carrousel.style.transform = `translateX(-${initial * cardWidth}px)`;
     }
 });
 
 arrowCarrouselLeft.addEventListener("click", ()=>{
+    let cardWidth = cards[0].offsetWidth; // Recalcula el ancho de la tarjeta
     if(initial > 0){
         initial--;
-        carrousel.style.transform = `translateX(-${initial * widthCarrousel}%)`;
+        carrousel.style.transform = `translateX(-${initial * cardWidth}px)`;
     }
 });
