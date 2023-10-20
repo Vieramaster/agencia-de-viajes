6607b30734df401fa6a83dc632868826ecf8131b
martin viera <118699912+Vieramaster@users.noreply.github.com>
2023-10-20 17:14:22 -0300

    working

--- lala.js
+++ lala.js
@@ -1,4 +1,51 @@
 
+/*MENU RESPONSIVE*/
+const  menuResponsive = document.querySelector(".box2")
+const btnOpen = document.querySelector("#btnOpen")
+const btnClose = document.querySelector("#btnClose")
+
+
+
+
+
+
+window.addEventListener("resize", ()=>{
+
+    if(window.innerWidth < 950){
+
+        btnOpen.style.display = "block"
+        btnClose.style.display = "none"
+        menuResponsive.style.transform = "translateY(-150%)";
+
+        btnOpen.addEventListener("click", ()=>{
+            btnOpen.style.display = "none"
+            btnClose.style.display = "block"
+            menuResponsive.style.transform = "translateY(0%)";
+            
+        })
+        
+        btnClose.addEventListener("click", ()=>{
+            btnOpen.style.display = "block"
+            btnClose.style.display = "none"
+            menuResponsive.style.transform = "translateY(-150%)";
+            
+        })
+        
+    }else{
+        btnOpen.style.display = "none"
+        btnClose.style.display = "none"
+        menuResponsive.style.transform = "translateY(0%)";
+    }
+})
+
+
+
+
+
+
+
+
+/* CARROUSEL */
 const carrousel = document.querySelector(".block2__carrousel")
 const cards = document.querySelectorAll(".block2__card")
 const arrowCarrouselLeft = document.querySelector("#leftArrow")
@@ -21,3 +68,4 @@ arrowCarrouselLeft.addEventListener("click", ()=>{
         carrousel.style.transform = `translateX(-${initial * 20}rem)`;
     }
 });
+
